d83c81006d0532d42d9d0eeb14cd9691b62607ad
Devarsh Bhatt <bhatt.devarsh17@gmail.com>
2023-12-30 17:11:38 +0530

    feat(app): User details doctype created

---
+++ training_app/training_app/doctype/user_details/test_user_details.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Devarsh Bhatt and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestUserDetails(FrappeTestCase):
+	pass
---
+++ training_app/training_app/doctype/user_details/user_details.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Devarsh Bhatt and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('User Details', {
+	// refresh: function(frm) {
+
+	// }
+});
